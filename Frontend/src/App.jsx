cc0989c75524041343ba5d6e1ab5a7932f27ed6d
jaganrajaa15-arch <jaganrajaa15@gmail.com>
2026-07-15 10:36:20 +0530

    pull

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,13 +5,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
-<<<<<<< HEAD
 import About from "./Pages/About/About";
 import Faqs from "./Pages/Faqs/Faqs";
 import ContactUs from "./Pages/ContactUs/ContactUs";
-=======
 import Footer from "./Components/Footer/Footer";
->>>>>>> 4d64763499523f3b64edbf01c74805e87974c223
 
 
 const App = () => {
@@ -23,7 +20,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About/>} />
-        <Route path="/pages/faqs" element={<Faqs/>} />
+        <Route path="/faq" element={<Faqs/>} />
         <Route path="/contact" element={<ContactUs/>} />
         
       </Routes>
